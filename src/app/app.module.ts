8df971020f263aca27d08d1b1d50989d50d796d2
carlosJr <carlosjraurgusto3@gmail.com>
2024-04-08 08:37:08 -0300

    desafio chuva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenusidebarComponent } from './menusidebar/menusidebar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FontComponent } from './font/font.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenusidebarComponent,
-    NavBarComponent
+    NavBarComponent,
+    FontComponent
   ],
   imports: [
     BrowserModule
